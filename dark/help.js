54a76517c9d1c0f0d92fc5751e9032126d60ddf2
nathanvlt123 <77707921+nathanvlt123@users.noreply.github.com>
2021-03-22 20:43:19 -0300

    Add files via upload

---
+++ dark/help.js
@@ -0,0 +1,356 @@
+const help = () => { 
+	return `
+🈳〘 INFO 〙🈳
+
+OLÁ base feita por dark
+
+➥ 🖤 𝔹𝕆𝕋-ℕ𝔸𝕋ℍ 🖤
+➥ 🖤2.0🖤
+➥ 𝐃𝐎𝐍𝐎: ℕ𝔸𝕋ℍ𝔸ℕ  🖤
+➥ 🖤wa.me/+5541999772028🖤
+➥ 𝐒𝐓𝐀𝐓𝐔𝐒: ON
+🈳〘 NOVIDADES 〙🈳
+
+ 🖤${prefix}msc🖤
+ 🖤${prefix}Comandos de voz🖤
+ 🖤${prefix}meme [corrigido]🖤
+ 🖤${prefix}comandos sem prefix🖤
+ 🖤${prefix}gbin [premium]🖤
+ 🖤${prefix}pack [premium]🖤
+ 🖤${prefix}destrava [premium]🖤
+ 🖤${prefix}gpessoa [premium]🖤
+ 🖤${prefix}wame🖤
+ 🖤${prefix}spamcall🖤
+ 🖤${prefix}play (nome da msc)🖤
+
+🈳〘 MENU 〙🈳
+
+ 🖤${prefix}figu🖤
+ 🖤${prefix}toimg🖤
+ 🖤${prefix}darkjokes (memes aleatórios)🖤
+ 🖤${prefix}memeindo🖤
+ 🖤${prefix}tts🖤
+ 🖤${prefix}lolih [on]🖤
+ 🖤${prefix}nsfwloli [off]🖤
+ 🖤${prefix}url2img🖤
+ 🖤${prefix}leens [na legenda]🖤
+ 🖤${prefix}wait [na legenda]🖤
+ 🖤${prefix}setprefix🖤
+
+🈳〘 OUTROS 〙🈳
+
+ 🖤${prefix}linkgp🖤
+ 🖤${prefix}simih [1/0]🖤
+ 🖤${prefix}marcar🖤
+ 🖤${prefix}add [@]🖤
+ 🖤${prefix}banir [@]🖤
+ 🖤${prefix}promover [@]🖤
+ 🖤${prefix}rebaixar🖤
+ 🖤${prefix}admins🖤
+ 🖤${prefix}marcar2🖤
+ 🖤${prefix}bc [texto]🖤 (ele faz uma ™)
+ 🖤${prefix}marcar3🖤
+ 🖤${prefix}bloqueados🖤
+ 🖤${prefix}bloquear [@]🖤
+ 🖤${prefix}desbloquear [@]🖤
+ 🖤${prefix}limpar🖤
+ 🖤${prefix}bc [ 🖤texto🖤 ]🖤
+ 🖤${prefix}bemvindo [1/0]🖤
+ 🖤${prefix}clonar [@]🖤
+ 🖤${prefix}help1🖤
+ 🖤${prefix}dono🖤
+ 🖤${prefix}owner🖤
+ 🖤${prefix}tts [texto]🖤
+ 🖤${prefix}setnome🖤
+ 🖤${prefix}termux🖤
+ 🖤${prefix}setfoto🖤
+ 🖤${prefix}grupoinfo🖤
+ 🖤${prefix}ytmp4🖤
+ 🖤${prefix}bomdia🖤
+ 🖤${prefix}boanoite🖤
+ 🖤${prefix}marcar🖤
+ 🖤${prefix}marcar2🖤
+ 🖤${prefix}marcar3🖤
+
+🈳〘 IMAGENS 〙🈳
+
+ 🖤${prefix}loli🖤 [off]
+ 🖤${prefix}loli1🖤
+ 🖤${prefix}hentai🖤
+ 🖤${prefix}dono🖤
+ 🖤${prefix}porno🖤
+ 🖤${prefix}boanoite🖤
+ 🖤${prefix}bomdia🖤
+ 🖤${prefix}boatarde🖤
+ 🖤${prefix}mia [aleatórias]🖤
+ 🖤${prefix}rize [aleatórias]🖤
+ 🖤${prefix}minato [aleatórias]🖤
+ 🖤${prefix}boruto [aleatórias]🖤
+ 🖤${prefix}hinata [aleatórias]🖤
+ 🖤${prefix}sasuke [aleatórias]🖤
+ 🖤${prefix}sakura [aleatórias]🖤
+ 🖤${prefix}naruto [aleatórias]🖤
+ 🖤${prefix}meme🖤   
+ 🖤${prefix}lofi🖤
+ 🖤${prefix}malkova🖤
+ 🖤${prefix}canal🖤
+ 🖤${prefix}nsfwloli1🖤
+ 🖤${prefix}reislin🖤
+
+🈳〘 INTELIGÊNCIA IA 〙🈳
+
+ 🖤${prefix}simih 1 (para ativar)🖤
+ 🖤${prefix}simih 0 (para desativar)🖤
+ 🖤${prefix}simi (sua mensagem)🖤
+
+🈳〘 EM TESTE 〙🈳
+
+ 🖤${prefix}🖤
+ 🖤${prefix}🖤
+ 🖤${prefix}🖤
+
+🌟〘 PREMIUM 〙🌟
+
+ 🖤${prefix}dado🖤
+ 🖤${prefix}cekvip🖤
+ 🖤${prefix}premiumlist🖤
+ 🖤${prefix}delete🖤
+ 🖤${prefix}modapk🖤
+ 🖤${prefix}indo10🖤
+ 🖤${prefix}daftarvip [para virar Premium]🖤
+ 🖤${prefix}qrcode🖤
+ 🖤${prefix}chentai🖤
+ 🖤${prefix}gcpf🖤
+ 🖤${prefix}gbin🖤
+ 🖤${prefix}pack🖤
+ 🖤${prefix}destrava🖤
+ 🖤${prefix}gpessoa🖤
+
+🈳〘 GRUPO 〙🈳
+
+ 🖤${prefix}banir🖤
+ 🖤${prefix}leveling [on/off]🖤
+ 🖤${prefix}level🖤
+ 🖤${prefix}add🖤
+ 🖤${prefix}promover🖤
+ 🖤${prefix}setfoto [na legenda]🖤
+ 🖤${prefix}setname [texto]🖤
+ 🖤${prefix}rebaixar🖤
+ 🖤${prefix}admins🖤
+ 🖤${prefix}marcar🖤
+ 🖤${prefix}marcar2🖤
+ 🖤${prefix}marcar3🖤
+ 🖤${prefix}bemvindo [1/0]🖤
+ 🖤${prefix}grupoinfo🖤
+ 🖤${prefix}bomdia🖤
+ 🖤${prefix}boatarde🖤
+ 🖤${prefix}boanoite🖤
+ 🖤${prefix}setdesc🖤
+ 🖤${prefix}bug [sua mensagem]🖤
+
+🈳〘 ESPECIFICO DO BOT 〙🈳
+
+ 🖤${prefix}bug [sua mensagem]🖤
+ 🖤${prefix}clonar [@]🖤
+ 🖤${prefix}dono🖤
+ 🖤${prefix}ping [ver velocidade do bot]🖤
+ 🖤${prefix}termux🖤
+ 🖤${prefix}gay [@]🖤
+ 🖤${prefix}wame🖤
+ 🖤${prefix}map (nome)🖤
+ 🖤${prefix}setppbot (marque uma img)🖤
+ 🖤${prefix}pinterest (nome)🖤
+ 🖤${prefix}desligar (so para o dono)🖤
+ 🖤${prefix}timer🖤
+
+🈳〘 MAIS ALGUNS 〙🈳
+
+ 🖤${prefix}neko🖤
+ 🖤${prefix}ttp [texto]🖤
+ 🖤${prefix}testime🖤
+ 🖤${prefix}tomp3🖤
+ 🖤${prefix}modoanime [on/off]🖤
+ 🖤${prefix}modonsfw [on/off]🖤
+ 🖤${prefix}happymod [jogo/app]🖤
+ 🖤${prefix}rize🖤
+ 🖤${prefix}ytsearch🖤
+ 🖤${prefix}moddroid [jogo/app]🖤
+ 🖤${prefix}xvideos [titulo]🖤🖤
+ 🖤${prefix}nomegp🖤
+ 🖤${prefix}darkjokes (memes aleatórios)🖤
+ 🖤${prefix}animecry🖤
+ 🖤${prefix}gay1🖤
+ 🖤${prefix}next🖤
+ 🖤${prefix}alerta🖤
+ 🖤${prefix}belle [img aleatórias]🖤
+ 🖤${prefix}pronomeneu [texto]🖤
+ 🖤${prefix}hobby🖤
+
+🈳〘 COMANDOS DE VOZ 〙🈳
+
+ 🖤${prefix}ola🖤
+ 🖤${prefix}bv🖤
+ 🖤${prefix}tchau🖤
+ 🖤${prefix}bem🖤
+ 🖤${prefix}a🖤
+ 🖤${prefix}fdp🖤
+ 🖤${prefix}onich🖤
+ 🖤${prefix}beat1🖤
+ 🖤${prefix}glub🖤
+
+🈳〘 OUTROS /2 〙🈳
+
+ 🖤${prefix}antilink [1/0]🖤
+ 🖤${prefix}brainly [pergunta]🖤
+ 🖤${prefix}antiracismo [on/off]🖤
+ 🖤${prefix}setnomebot🖤
+ 🖤${prefix}meme🖤
+ 🖤${prefix}musicas🖤
+
+🈳〘 YT MUSIC 〙🈳
+
+══NOTA »
+menu completo em .msc
+══════════════════
+
+ 🖤${prefix}hylander🖤
+ 🖤${prefix}nikeb🖤
+ 🖤${prefix}BanhoDeLeite🖤
+ 🖤${prefix}AkDoFlamengo🖤
+ 🖤${prefix}Nemo🖤
+ 🖤${prefix}kalidade🖤
+ 🖤${prefix}macaverde🖤
+ 🖤${prefix}m4🖤
+ 🖤${prefix}cmgremio🖤
+ 🖤${prefix}cmflamengo🖤
+ 🖤${prefix}mercury🖤
+ 🖤${prefix}ponta🖤
+ 🖤${prefix}moça🖤
+ 🖤${prefix}nav🖤
+ 🖤${prefix}dama🖤
+ 🖤${prefix}remedy🖤
+ 🖤${prefix}cavala🖤
+ 🖤${prefix}facetime🖤
+ 🖤${prefix}pe🖤
+ 🖤${prefix}nike12🖤
+
+
+🈳〘 HACKER MENU 〙🈳
+
+ 🖤${prefix}fbcheker🖤
+ 🖤${prefix}hackfb🖤
+ 🖤${prefix}bruteforcefb🖤
+ 🖤${prefix}toolsphishing🖤
+ 🖤${prefix}terkeytermux🖤
+ 🖤${prefix}socialfishv2🖤
+ 🖤${prefix}tool🖤
+
+🈳〘 STICKER MENU 〙🈳
+
+ 🖤${prefix}animecry🖤 
+ 🖤${prefix}hubgif🖤
+ 🖤${prefix}blowgif🖤 🔞
+ 🖤${prefix}shota🖤
+ 🖤${prefix}hentaifig🖤 🔞
+ 🖤${prefix}nekofig🖤 🔞
+ 🖤${prefix}lolifig🖤
+
+🈳〘 INTERATIVOS 〙🈳
+
+══NOTA »
+Mandar a msg sem o prefixo
+════════════════════
+
+ 🖤bah🖤
+ 🖤oii🖤
+ 🖤bv🖤
+ 🖤canta ai bot🖤
+ 🖤grita🖤
+ 🖤digita🖤
+ 🖤machista🖤
+
+🈳〘 OUTROS /3 〙🈳
+
+ 🖤${prefix}atris🖤 🔞
+ 🖤${prefix}animemenu🖤
+
+🈳〘 CMDS DE VÍDEO 〙🈳
+
+══EDITS »
+_🖤Say Cat🖤_
+════════
+
+ 🖤${prefix}say1🖤
+ 🖤${prefix}say2🖤
+ 🖤${prefix}say3🖤
+ 🖤${prefix}say4🖤
+
+══EDITS »
+_🖤XXXTENTACIONS🖤_
+═══════════════
+
+ 🖤${prefix}xxx1🖤
+ 🖤${prefix}xxx2🖤
+
+══EDITS »
+_🖤VARIADAS🖤_
+═══════════════
+
+ 🖤${prefix}teto1🖤
+ 🖤${prefix}edit1🖤
+ 🖤${prefix}edit2🖤
+ 🖤${prefix}edit3🖤
+ 🖤${prefix}edit4🖤
+═══════════════
+
+🈳〘 CONSULTAS E OUTROS 〙🈳
+
+
+ 🖤${prefix}iplog (ip)🖤 [consulta ip]
+ 🖤${prefix}cpfg🖤 [bot gera cpf]
+ 🖤${prefix}bcep (cep)🖤 [consulta cep]
+
+🈳〘 LOGOS 〙🈳
+
+ 🖤${prefix}pslogo (seu texto)🖤
+ 🖤${prefix}milogo (seu texto)🖤
+ 🖤${prefix}aguia (seu texto)🖤
+ 🖤${prefix}monkey (seu texto)🖤
+ 🖤${prefix}dnulis (seu texto)🖤
+ 🖤${prefix}dmeme (seu texto)🖤
+ 🖤${prefix}dnobg (seu texto)🖤
+ 🖤${prefix}dnobg2 (seu texto/seu texto)🖤
+ 🖤${prefix}tlogo (seu texto)🖤
+ 🖤${prefix}nlogo (seu texto)🖤
+ 🖤${prefix}black (seu texto)🖤
+ 🖤${prefix}clogo (seu texto)🖤
+ 🖤${prefix}glogo (seu texto/seu texto)🖤
+ 🖤${prefix}aguia2 (seu texto/seu texto)🖤
+ 🖤${prefix}wlogo (seu texto)🖤
+ 🖤${prefix}letxt (seu texto)🖤
+ 🖤${prefix}text3d (seu texto)🖤
+ 🖤${prefix}amor (tamanho/blush)🖤 [numeros]
+ 🖤${prefix}frase (seu texto/seu texto)🖤
+ 🖤${prefix}canal (seu texto)🖤
+ 🖤${prefix}plogo (seu texto)🖤 [plaquinha]
+
+🈳〘 MUSIC GEEK 〙🈳
+
+ 🖤${prefix}minato1
+ 🖤${prefix}minato2
+ 🖤${prefix}deidara
+ 🖤${prefix}hashirama
+ 🖤${prefix}pain
+ 🖤${prefix}vegeto
+ 🖤${prefix}gogeta
+ 🖤${prefix}eren
+ 🖤${prefix}renegado
+ 🖤${prefix}tobi
+ 🖤${prefix}vibea
+ 🖤${prefix}tobirama
+  
+ 🖤${prefix}dono1🖤 para mais info`
+}
+
+exports.help = help
+
